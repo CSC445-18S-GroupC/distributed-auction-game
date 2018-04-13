7d3f0bd93a27db332e54ef46b1b8422f36ed4e4e
Christopher Wells <cwellsny@nycap.rr.com>
2018-04-13 10:15:44 -0400

    Create project structure

---
+++ src/main/java/csc445/groupc/distauction/Main.java
@@ -0,0 +1,10 @@
+package csc445.groupc.distauction;
+
+/**
+ * Created by chris on 4/13/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
