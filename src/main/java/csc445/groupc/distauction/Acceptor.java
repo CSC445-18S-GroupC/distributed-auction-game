3c42fa7bd178d50420a5ea76c7d169b8328b8a18
Christopher Wells <cwellsny@nycap.rr.com>
2018-04-28 15:36:05 -0400

    Start working on high level logic for Acceptor

---
+++ src/main/java/csc445/groupc/distauction/Acceptor.java
@@ -0,0 +1,92 @@
+package csc445.groupc.distauction;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by chris on 4/28/18.
+ */
+public class Acceptor {
+    private static final Integer REQUEST = 9;
+    private static final Integer PREPARE = 0;
+    private static final Integer PROMISE_NO_VALUE = 1;
+    private static final Integer PROMISE_WITH_VALUE = 2;
+    private static final Integer ACCEPT_REQUEST = 3;
+    private static final Integer ACCEPT = 4;
+
+    // TODO: Change to actual messages
+    private final LinkedBlockingQueue<Integer> messageQueue;
+    private final LinkedBlockingQueue<Integer> sendQueue;
+
+    /**
+     * A value indicating if the Acceptor should continue running. It is set to true when run() is called, and then is
+     * set to be false when shutdown() is called, in order to stop the run() loop.
+     */
+    private final AtomicBoolean running;
+
+    public Acceptor(final LinkedBlockingQueue<Integer> messageQueue, final LinkedBlockingQueue<Integer> sendQueue) {
+        this.messageQueue = messageQueue;
+        this.sendQueue = sendQueue;
+
+        this.running = new AtomicBoolean(false);
+    }
+
+    public void run() throws InterruptedException {
+        running.set(true);
+
+        while (running.get()) {
+            // TODO: Change to use real messages
+            final Integer message = messageQueue.take();
+
+            // TODO: Update to work with actual messages
+            if (message.equals(PREPARE)) {
+                final int proposalId = 5;
+
+                if (!proposalIsObolete(proposalId)) {
+                    if (alreadyHaveAProposal()) {
+                        sendPromiseWithPreviousValue(proposalId);
+                    } else {
+                        sendRegularPromise(proposalId);
+                    }
+                }
+            } else if (message.equals(ACCEPT_REQUEST)) {
+                final int proposalId = 5;
+
+                if (!proposalIsObolete(proposalId)) {
+                    sendAcceptToProposer(proposalId);
+                    sendAcceptToAllLearners(proposalId);
+                }
+            }
+        }
+    }
+
+    public void shutdown() {
+        running.set(false);
+    }
+
+    private boolean proposalIsObolete(final int proposalId) {
+        // TODO: Implement
+        return false;
+    }
+
+    private boolean alreadyHaveAProposal() {
+        // TODO: Implement
+        return false;
+    }
+
+    private void sendPromiseWithPreviousValue(final int proposalId) {
+        // TODO: Implement
+    }
+
+    private void sendRegularPromise(final int proposalId) {
+        // TODO: Implement
+    }
+
+    private void sendAcceptToProposer(final int proposalId) {
+        // TODO: Implement
+    }
+
+    private void sendAcceptToAllLearners(final int proposalId) {
+        // TODO: Implement
+    }
+}
